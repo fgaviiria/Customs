6f99f23e78858356966df5814d34a5ffdb189a22
inteligosfranck <fagaviria1@gmail.com>
2024-11-26 10:08:02 -0400

    Cambios

--- pos_fg/models/account_move.py
+++ pos_fg/models/account_move.py
@@ -8,20 +8,24 @@ import qrcode
 class AccountMove(models.Model):
     _inherit = 'account.move'
 
-    serial_number = fields.Char(string="Serial Number", compute='_serial', readonly=False)
-    correlative_number = fields.Char(string="Correlative Number", compute='_correlative', readonly=False)
+    serial_number = fields.Char(string="Serial Number", compute='_serial', readonly=True)
+    correlative_number = fields.Char(string="Correlative Number", compute='_correlative', readonly=True)
+    qr_str = fields.Char(compute='_generate_str')
 
-    @api.depends('payment_reference')
+    @api.depends()
     def _serial(self):
-        serial_number = self.payment_reference.split('/')
+        self.serial_number = self.payment_reference.split('/')[0] + self.payment_reference.split('/')[1]
 
-    @api.depends('payment_reference')
+    @api.depends()
     def _correlative(self):
-        correlative_number = self.payment_reference.split()
+        self.correlative_number = self.payment_reference.split('/')[2]
+
+    def _generate_str(self):
+        self.qr_str = self.name + '| ' + self.partner_id.name + '| ' + str(self.invoice_date) + '| ' + str(self.amount_residual) + '| ' + str(self.amount_total)
 
     def generate_qr_code(self):
         qr = qrcode.QRCode(version=4, box_size=4, border=1)
-        qr.add_data(self)
+        qr.add_data(self.qr_str)
         qr.make(fit=True)
         img = qr.make_image()
         buffer = io.BytesIO()
